9073518190965834ff3a802862f2fd046964bb88
tmori0959 <tmori.tmori0959@gmail.com>
2022-01-13 22:15:30 +0900

    Initial commit

---
+++ app/src/main/java/jp/techacademy/toshio/mori/kotlinlog/MainActivity.kt
@@ -0,0 +1,37 @@
+package jp.techacademy.toshio.mori.kotlinlog
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log     //追加2022.01.12
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        Log.d("kotlintest", "ログへの出力テスト")    //追加2022.01.12
+
+        // 整数型の変数をnumという名前で作成して、10を代入する
+        var num = 10
+        Log.d("kotlintest", Integer.toString(num))
+
+        // numに50を代入する
+        // 変数や定数に .toString() と記述する方法もある
+        num = 50
+        Log.d("kotlintest", num.toString())
+
+        val num1 = 10 + 5 - 2 * 4 / 2
+        Log.d("kotlintest", "10 + 5 -2 * 4 / 2 = " + num1)
+
+        val flag1 = true
+        val flag2 = false
+        Log.d("kotlintest", "flag1 && flag2 = " + (flag1 && flag2))
+        Log.d("kotlintest", "flag1 || flag2 = " + (flag1 || flag2))
+
+        val num2 = 10
+        val num3 = 20
+        Log.d("kotlintest", "num2 < num3 = " + (num2 < num3))
+
+    }
+}
+
